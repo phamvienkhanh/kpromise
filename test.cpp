8e44169fe935bd535609939ef5f12ab0e0fa84b7
keith <keith@example.com>
2021-09-13 04:55:09 -0400

    ! update test

--- test.cpp
+++ test.cpp
@@ -2,20 +2,45 @@
 #include "include/promise.hpp"
 #include <iostream>
 
+auto asyncFunc() -> kasync::Promise {
+    return kasync::Promise([](kasync::Resolve& resolve, kasync::Reject& reject) {
+        std::cout << "===================\n";
+        std::cout << "==== func async ====\n";
+        std::cout << "= thread id: " << std::this_thread::get_id() << std::endl;
+        std::cout << "===================";
+        resolve(123);
+    });
+}
+
 auto main() -> int {
 
     kasync::PromiseExecutor::init();
 
-    kasync::Promise p([](kasync::Resolve& resolve, kasync::Reject& reject) {
-        std::any a = 123;
-        resolve(a);
-    });
-    p.then([](std::any val) -> std::any {
-        std::cout << "==== then ====\n"; 
-        std::cout << std::any_cast<int>(val) << std::endl;
+    std::cout << "===================\n";
+    std::cout << "==== main ====\n";
+    std::cout << "= thread id: " << std::this_thread::get_id() << std::endl;
+    std::cout << "===================";
+
+    asyncFunc()
+    .then([](std::any val) -> std::any {
+        auto data = val.has_value() ? std::any_cast<int>(val) : 0;
+        std::cout << "===================\n";
+        std::cout << "==== then 1 ====\n";
+        std::cout << "= thread id: " << std::this_thread::get_id() << std::endl;
+        std::cout << "= value: " << data << std::endl;
+        std::cout << "===================";
         return {};
-    });
-    p.launch();
+    })
+    .then([](std::any val) -> std::any {
+        auto data = val.has_value() ? std::any_cast<int>(val) : 0;
+        std::cout << "===================\n";
+        std::cout << "==== then 2 ====\n";
+        std::cout << "= thread id: " << std::this_thread::get_id() << std::endl;
+        std::cout << "= value: " << data << std::endl;
+        std::cout << "===================";
+        return {};
+    })
+    .launch();
 
     while(1) {
         kasync::PromiseHandler::getIntance()->callNext();
